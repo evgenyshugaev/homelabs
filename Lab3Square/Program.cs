a1a29ad93d4309ac86ead87727f604e251a98bb6
eshugaev <dotnet.dev2@gmail.com>
2022-06-21 09:13:45 +0300

    changes

--- Lab3Square/Program.cs
+++ Lab3Square/Program.cs
@@ -6,7 +6,7 @@ namespace Lab3Square
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("For testing SquareRoot use ");
         }
     }
 }
